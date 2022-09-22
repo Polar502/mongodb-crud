8a4bd67de8f165f0eb7e12de28c83a37fc1ab12e
Polar502 <marioarita502@gmail.com>
2022-09-21 23:42:48 -0600

    config.js

--- src/config.js
+++ src/config.js
@@ -3,6 +3,6 @@ import { config } from "dotenv";
 config();
 
 export const MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/test";
+  process.env.MONGODB_URI;
 
 export const PORT = process.env.PORT;
